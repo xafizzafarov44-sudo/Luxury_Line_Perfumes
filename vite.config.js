386d23b8f8c82a0763be3401a1763688906f28e1
xafizzafarov44-sudo <xafizzafarov44@gmail.com>
2026-02-02 01:24:33 +0500

    zzzzzzzzzzz

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Luxury_Line_Perfumes/",
   plugins: [react(),tailwindcss(),],
 })
